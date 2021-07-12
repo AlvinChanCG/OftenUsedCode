139851d8866eafe35a6e1c330da7ee1049cd9097
cgh <44086116+HELL-TO-HEAVEN@users.noreply.github.com>
2021-07-12 14:01:33 +0800

    Initial commit

---
+++ SetRandomSeed/setup_seed.py
@@ -0,0 +1,12 @@
+import numpy as np
+import torch
+import random
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+# 设置随机数种子
+setup_seed(20)
